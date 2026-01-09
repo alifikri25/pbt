c0e746be2095f16eb34257e9e3e7b1ec1ae93b90
alifikri25 <aliifikriii25@gmail.com>
2026-01-10 02:54:53 +0700

    Fix slot positions

--- src/data/frames.js
+++ src/data/frames.js
@@ -1,6 +1,19 @@
-export const FRAME_CATEGORIES = ['All', 'Aesthetic', 'Vintage', 'Cute', 'Minimal', 'Fun', 'Romantic'];
+export const FRAME_CATEGORIES = ['All', 'Classic', 'Aesthetic', 'Cute', 'Vintage'];
 
 export const FRAMES = [
+    {
+        id: 'clean-film',
+        name: 'Film Strip',
+        category: 'Classic',
+        image: '/frames/clean_film.png',
+        thumbnail: '/frames/clean_film.png',
+        photoSlots: [
+            { x: 164, y: 22, width: 185, height: 105 },
+            { x: 164, y: 138, width: 185, height: 105 },
+            { x: 164, y: 254, width: 185, height: 105 },
+            { x: 164, y: 370, width: 185, height: 105 },
+        ]
+    },
     {
         id: 'denim-stickers',
         name: 'Denim Stickers',
@@ -8,49 +21,36 @@ export const FRAMES = [
         image: '/frames/denim.png',
         thumbnail: '/frames/denim.png',
         photoSlots: [
-            { x: 180, y: 62, width: 152, height: 105 },
-            { x: 180, y: 180, width: 152, height: 105 },
-            { x: 180, y: 298, width: 152, height: 105 },
-            { x: 180, y: 416, width: 152, height: 105 },
+            { x: 205, y: 43, width: 100, height: 80 },
+            { x: 205, y: 143, width: 100, height: 80 },
+            { x: 205, y: 243, width: 100, height: 80 },
+            { x: 205, y: 343, width: 100, height: 80 },
         ]
     },
     {
         id: 'nutrition-label',
         name: 'Nutrition Label',
-        category: 'Fun',
+        category: 'Vintage',
         image: '/frames/nutrition.png',
         thumbnail: '/frames/nutrition.png',
         photoSlots: [
-            { x: 168, y: 150, width: 175, height: 95 },
-            { x: 168, y: 258, width: 175, height: 95 },
-            { x: 168, y: 366, width: 175, height: 95 },
-            { x: 168, y: 474, width: 175, height: 95 },
+            { x: 169, y: 118, width: 175, height: 85 },
+            { x: 169, y: 218, width: 175, height: 85 },
+            { x: 169, y: 318, width: 175, height: 85 },
+            { x: 169, y: 418, width: 175, height: 85 },
         ]
     },
     {
         id: 'every-moment',
         name: 'Every Moment',
-        category: 'Minimal',
+        category: 'Classic',
         image: '/frames/every_moment.png',
         thumbnail: '/frames/every_moment.png',
         photoSlots: [
-            { x: 175, y: 70, width: 166, height: 95 },
-            { x: 175, y: 178, width: 166, height: 95 },
-            { x: 175, y: 286, width: 166, height: 95 },
-            { x: 175, y: 394, width: 166, height: 95 },
-        ]
-    },
-    {
-        id: 'vintage-inc',
-        name: 'Photomat Inc.',
-        category: 'Vintage',
-        image: '/frames/vintage_inc.png',
-        thumbnail: '/frames/vintage_inc.png',
-        photoSlots: [
-            { x: 162, y: 90, width: 188, height: 110 },
-            { x: 162, y: 210, width: 188, height: 110 },
-            { x: 162, y: 330, width: 188, height: 110 },
-            { x: 162, y: 450, width: 188, height: 110 },
+            { x: 175, y: 55, width: 158, height: 92 },
+            { x: 175, y: 160, width: 158, height: 92 },
+            { x: 175, y: 265, width: 158, height: 92 },
+            { x: 175, y: 370, width: 158, height: 92 },
         ]
     },
     {
@@ -60,49 +60,10 @@ export const FRAMES = [
         image: '/frames/korean_cute.png',
         thumbnail: '/frames/korean_cute.png',
         photoSlots: [
-            { x: 176, y: 53, width: 160, height: 100 },
-            { x: 176, y: 165, width: 160, height: 100 },
-            { x: 176, y: 277, width: 160, height: 100 },
-            { x: 176, y: 389, width: 160, height: 100 },
-        ]
-    },
-    {
-        id: 'retro-film',
-        name: 'Kodak Film',
-        category: 'Vintage',
-        image: '/frames/film.png',
-        thumbnail: '/frames/film.png',
-        photoSlots: [
-            { x: 192, y: 48, width: 128, height: 95 },
-            { x: 192, y: 158, width: 128, height: 95 },
-            { x: 192, y: 268, width: 128, height: 95 },
-            { x: 192, y: 378, width: 128, height: 95 },
-        ]
-    },
-    {
-        id: 'receipt-style',
-        name: 'Memory Store',
-        category: 'Fun',
-        image: '/frames/receipt.png',
-        thumbnail: '/frames/receipt.png',
-        photoSlots: [
-            { x: 165, y: 85, width: 182, height: 100 },
-            { x: 165, y: 215, width: 182, height: 100 },
-            { x: 165, y: 345, width: 182, height: 100 },
-            { x: 165, y: 475, width: 182, height: 100 },
-        ]
-    },
-    {
-        id: 'magazine-cover',
-        name: 'Vogue Style',
-        category: 'Aesthetic',
-        image: '/frames/magazine.png',
-        thumbnail: '/frames/magazine.png',
-        photoSlots: [
-            { x: 82, y: 158, width: 150, height: 125 },
-            { x: 280, y: 158, width: 150, height: 125 },
-            { x: 82, y: 308, width: 150, height: 125 },
-            { x: 280, y: 308, width: 150, height: 125 },
+            { x: 178, y: 38, width: 155, height: 95 },
+            { x: 178, y: 148, width: 155, height: 95 },
+            { x: 178, y: 258, width: 155, height: 95 },
+            { x: 178, y: 368, width: 155, height: 95 },
         ]
     },
     {
@@ -112,23 +73,49 @@ export const FRAMES = [
         image: '/frames/ticket.png',
         thumbnail: '/frames/ticket.png',
         photoSlots: [
-            { x: 160, y: 108, width: 192, height: 95 },
-            { x: 160, y: 228, width: 192, height: 95 },
-            { x: 160, y: 348, width: 192, height: 95 },
-            { x: 160, y: 468, width: 192, height: 95 },
+            { x: 164, y: 88, width: 185, height: 90 },
+            { x: 164, y: 198, width: 185, height: 90 },
+            { x: 164, y: 308, width: 185, height: 90 },
+            { x: 164, y: 418, width: 185, height: 90 },
         ]
     },
     {
-        id: 'y2k-cyber',
-        name: '2000s Vibes',
-        category: 'Aesthetic',
-        image: '/frames/y2k.png',
-        thumbnail: '/frames/y2k.png',
+        id: 'love-hearts',
+        name: 'Love Hearts',
+        category: 'Cute',
+        image: '/frames/love_hearts.png',
+        thumbnail: '/frames/love_hearts.png',
         photoSlots: [
-            { x: 176, y: 82, width: 160, height: 90 },
-            { x: 176, y: 186, width: 160, height: 90 },
-            { x: 176, y: 290, width: 160, height: 90 },
-            { x: 176, y: 394, width: 160, height: 90 },
+            { x: 178, y: 62, width: 158, height: 92 },
+            { x: 178, y: 170, width: 158, height: 92 },
+            { x: 178, y: 278, width: 158, height: 92 },
+            { x: 178, y: 386, width: 158, height: 92 },
+        ]
+    },
+    {
+        id: 'retro-arcade',
+        name: 'Insert Coin',
+        category: 'Aesthetic',
+        image: '/frames/arcade.png',
+        thumbnail: '/frames/arcade.png',
+        photoSlots: [
+            { x: 160, y: 50, width: 192, height: 92 },
+            { x: 160, y: 160, width: 192, height: 92 },
+            { x: 160, y: 270, width: 192, height: 92 },
+            { x: 160, y: 380, width: 192, height: 92 },
+        ]
+    },
+    {
+        id: 'receipt-style',
+        name: 'Memory Store',
+        category: 'Vintage',
+        image: '/frames/receipt.png',
+        thumbnail: '/frames/receipt.png',
+        photoSlots: [
+            { x: 168, y: 62, width: 178, height: 95 },
+            { x: 168, y: 182, width: 178, height: 95 },
+            { x: 168, y: 302, width: 178, height: 95 },
+            { x: 168, y: 422, width: 178, height: 95 },
         ]
     },
     {
@@ -138,10 +125,10 @@ export const FRAMES = [
         image: '/frames/neon_glow.png',
         thumbnail: '/frames/neon_glow.png',
         photoSlots: [
-            { x: 152, y: 72, width: 208, height: 92 },
-            { x: 152, y: 180, width: 208, height: 92 },
-            { x: 152, y: 288, width: 208, height: 92 },
-            { x: 152, y: 396, width: 208, height: 92 },
+            { x: 155, y: 58, width: 200, height: 88 },
+            { x: 155, y: 162, width: 200, height: 88 },
+            { x: 155, y: 266, width: 200, height: 88 },
+            { x: 155, y: 370, width: 200, height: 88 },
         ]
     },
     {
@@ -151,61 +138,22 @@ export const FRAMES = [
         image: '/frames/newspaper.png',
         thumbnail: '/frames/newspaper.png',
         photoSlots: [
-            { x: 160, y: 95, width: 195, height: 85 },
-            { x: 160, y: 205, width: 195, height: 85 },
-            { x: 160, y: 315, width: 195, height: 85 },
-            { x: 160, y: 425, width: 195, height: 85 },
-        ]
-    },
-    {
-        id: 'love-hearts',
-        name: 'Love Hearts',
-        category: 'Romantic',
-        image: '/frames/love_hearts.png',
-        thumbnail: '/frames/love_hearts.png',
-        photoSlots: [
-            { x: 178, y: 80, width: 156, height: 95 },
-            { x: 178, y: 190, width: 156, height: 95 },
-            { x: 178, y: 300, width: 156, height: 95 },
-            { x: 178, y: 410, width: 156, height: 95 },
-        ]
-    },
-    {
-        id: 'retro-arcade',
-        name: 'Insert Coin',
-        category: 'Fun',
-        image: '/frames/arcade.png',
-        thumbnail: '/frames/arcade.png',
-        photoSlots: [
-            { x: 158, y: 60, width: 196, height: 100 },
-            { x: 158, y: 175, width: 196, height: 100 },
-            { x: 158, y: 290, width: 196, height: 100 },
-            { x: 158, y: 405, width: 196, height: 100 },
+            { x: 168, y: 82, width: 178, height: 78 },
+            { x: 168, y: 188, width: 178, height: 78 },
+            { x: 168, y: 294, width: 178, height: 78 },
+            { x: 168, y: 400, width: 178, height: 78 },
         ]
     },
     {
         id: 'botanical-minimal',
-        name: 'Forever & Always',
-        category: 'Minimal',
+        name: 'Forever Always',
+        category: 'Classic',
         image: '/frames/botanical.png',
         thumbnail: '/frames/botanical.png',
         photoSlots: [
-            { x: 178, y: 128, width: 156, height: 105 },
-            { x: 178, y: 253, width: 156, height: 105 },
-            { x: 178, y: 378, width: 156, height: 105 },
-        ]
-    },
-    {
-        id: 'polaroid-vintage',
-        name: 'Summer Memories',
-        category: 'Romantic',
-        image: '/frames/polaroid.png',
-        thumbnail: '/frames/polaroid.png',
-        photoSlots: [
-            { x: 198, y: 85, width: 118, height: 88 },
-            { x: 198, y: 195, width: 118, height: 88 },
-            { x: 198, y: 305, width: 118, height: 88 },
-            { x: 198, y: 415, width: 118, height: 88 },
+            { x: 182, y: 120, width: 148, height: 100 },
+            { x: 182, y: 240, width: 148, height: 100 },
+            { x: 182, y: 360, width: 148, height: 100 },
         ]
     },
 ];
